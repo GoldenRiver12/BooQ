f244d1ef1804fd5ff729ea47c5019445df144625
GoldenRiver12 <gandhi9736@gmail.com>
2022-01-09 14:22:45 +0900

    [add] 質問モデル作成

---
+++ src/main/java/com/example/demo/model/Question.java
@@ -0,0 +1,51 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+import org.hibernate.validator.constraints.ISBN;
+
+import lombok.*;
+
+/**
+ * 質問のモデル
+ * @author Kanagawa Tatsuki
+ *
+ */
+@Data
+@EqualsAndHashCode
+@Entity
+public class Question implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	
+	/**
+	 * ユーザーID
+	 */
+	@Id
+	private String userId;
+	
+	/**
+	 * 質問ID
+	 */
+	@Id
+	private String questionId;
+	
+	/**
+	 * 質問対象の本のISBM
+	 */
+	@ISBN
+	private String isbn;
+	
+	/**
+	 * 質問の投稿日時
+	 */
+	private LocalDateTime registrationTime;
+	
+	/**
+	 * 質問内容
+	 */
+	private String content;
+}
