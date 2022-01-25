56370720e5e0549d5e3eee334093ab472ba7122f
GoldenRiver12 <gandhi9736@gmail.com>
2022-01-25 17:35:38 +0900

    [fix] ISBNリスト＆キーワードで検索するメソッドのJPQLを修正

--- src/main/java/com/example/demo/repository/QuestionRepository.java
+++ src/main/java/com/example/demo/repository/QuestionRepository.java
@@ -1,10 +1,18 @@
 package com.example.demo.repository;
 
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.example.demo.model.Question;
 import com.example.demo.model.QuestionId;
 
-public interface QuestionRepository extends JpaRepository<Question,QuestionId>{
+public interface QuestionRepository extends JpaRepository<Question, QuestionId> {
+	// @Query("select * from question where isbn13 in isbnlist and ")
 
+	@Query("SELECT q from Question q WHERE isbn IN isbnlist AND content LIKE '%keyword%'")
+	public List<Question> findQuestionByIsbmListAndKeyword(@Param("isbnlist") List<String> isbnList, @Param("keyword") String keyword);
 }
