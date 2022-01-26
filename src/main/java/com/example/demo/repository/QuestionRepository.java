771a2b9d304e5c11d7281bdb04a6477ddeba4390
GoldenRiver12 <gandhi9736@gmail.com>
2022-01-26 15:56:07 +0900

    [fix] ISBN配列と質問内容の部分一致からなる検索メソッドのSQLを修正

--- src/main/java/com/example/demo/repository/QuestionRepository.java
+++ src/main/java/com/example/demo/repository/QuestionRepository.java
@@ -1,8 +1,6 @@
 package com.example.demo.repository;
 
 import java.util.List;
-import java.util.Set;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,8 +9,7 @@ import com.example.demo.model.Question;
 import com.example.demo.model.QuestionId;
 
 public interface QuestionRepository extends JpaRepository<Question, QuestionId> {
-	// @Query("select * from question where isbn13 in isbnlist and ")
-
-	@Query("SELECT q from Question q WHERE isbn IN isbnlist AND content LIKE '%keyword%'")
+	
+	@Query("SELECT q from Question q WHERE isbn IN :isbnlist AND content LIKE CONCAT('%', :keyword, '%')")
 	public List<Question> findQuestionByIsbmListAndKeyword(@Param("isbnlist") List<String> isbnList, @Param("keyword") String keyword);
 }
