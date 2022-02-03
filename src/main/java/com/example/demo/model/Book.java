ae27a2bbc09f710e17439411173d9ea5dd556993
GoldenRiver12 <gandhi9736@gmail.com>
2022-02-03 14:14:18 +0900

    [modify] 本モデルにbuilderを追加

--- src/main/java/com/example/demo/model/Book.java
+++ src/main/java/com/example/demo/model/Book.java
@@ -1,9 +1,13 @@
 package com.example.demo.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
 @AllArgsConstructor
 public class Book {
 	private String isbn13;
