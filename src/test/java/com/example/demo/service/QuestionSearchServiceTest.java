0a58038ce0d8ad4b1abe67bd4d0b951b2b451063
GoldenRiver12 <gandhi9736@gmail.com>
2022-02-03 14:15:39 +0900

    [add] 質問検索Serviceの検索メソッドに対するテストを作成

---
+++ src/test/java/com/example/demo/service/QuestionSearchServiceTest.java
@@ -0,0 +1,102 @@
+package com.example.demo.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import org.aspectj.lang.annotation.Before;
+import org.hamcrest.beans.SamePropertyValuesAs;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.model.Book;
+import com.example.demo.model.Question;
+import com.example.demo.model.SiteUser;
+import com.example.demo.repository.BookRepository;
+import com.example.demo.repository.QuestionRepository;
+import com.example.demo.repository.SiteUserRepository;
+import com.example.demo.util.Role;
+
+@SpringBootTest
+@Transactional
+class QuestionSearchServiceTest {
+
+	@Autowired
+	SiteUserRepository siteUserRepository;
+
+	@Autowired
+	QuestionRepository questionRepository;
+
+	@Mock
+	BookRepository bookRepository;
+
+	@Autowired
+	@InjectMocks
+	QuestionSearchService questionSearchService;
+
+	@BeforeEach
+	void setup() {
+		MockitoAnnotations.openMocks(bookRepository);
+	}
+
+	@Test
+	void 質問内容が質問検索キーワードを含み＿本が本検索キーワードを含むとき＿その質問を取得する() throws IOException {
+		
+		SiteUser user = SiteUser.builder()
+			.username("田中 太郎")
+			.password("password")
+			.email("tanakataro@example.com")
+			.role(Role.ROLE_USER.toString())
+			.build();
+
+		siteUserRepository.save(user);
+
+		Long userid = siteUserRepository.findByUsername("田中 太郎")
+			.get()
+			.getUserid();
+
+		Book book = Book.builder()
+			.isbn13("9784492470855")
+			.thumbnail("http://example.com/test")
+			.title("入門熱力学")
+			.build();
+
+		Question question = Question.builder()
+			.isbn("9784492470855")
+			.userid(userid)
+			.registrationTime(LocalDateTime.now())
+			.content("熱力学の第一法則とは何か？")
+			.build();
+
+		
+		siteUserRepository.save(user);
+		questionRepository.save(question);
+		when(bookRepository.findByKeyword("熱力学")).thenReturn(List.of(book));
+
+		
+		List<Question> actual = questionSearchService.findQuestionByBookKeywordAndQuestionKeyword("熱力学", "第一法則");
+		
+		
+		assertThat(List.of(question)).usingRecursiveComparison()
+			.ignoringFields("questionId")
+			.ignoringCollectionOrder()
+			.isEqualTo(actual);
+	}
+
+}
