b282e0029d4e748a7672459edfa00e4131ed23b6
GoldenRiver12 <gandhi9736@gmail.com>
2022-02-11 15:57:25 +0900

    [add] 質問検索サービスを仮作成

---
+++ src/main/java/com/example/demo/service/searchquestionService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Question;
+
+public class searchquestionService {
+	public List<Question> findQuestionByQuestionKeyword(String questionKeyword){
+		return List.of();
+	}
+}
