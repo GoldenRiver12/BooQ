4f7ab61f33ec1f9be6935f8d39b923a193c097d2
GoldenRiver12 <gandhi9736@gmail.com>
2022-02-16 20:05:51 +0900

    [add] @Serviceの追加

--- src/main/java/com/example/demo/service/searchquestionService.java
+++ src/main/java/com/example/demo/service/searchquestionService.java
@@ -2,8 +2,11 @@ package com.example.demo.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.model.Question;
 
+@Service
 public class searchquestionService {
 	public List<Question> findQuestionByQuestionKeyword(String questionKeyword){
 		return List.of();
