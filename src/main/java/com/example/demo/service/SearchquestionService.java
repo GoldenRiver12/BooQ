e3fa0ed1b8f7400042291bac43b08bfe42022542
GoldenRiver12 <gandhi9736@gmail.com>
2022-02-16 20:41:21 +0900

    [add] 質問検索ロジックを作成

--- src/main/java/com/example/demo/service/SearchquestionService.java
+++ src/main/java/com/example/demo/service/SearchquestionService.java
@@ -1,14 +1,40 @@
 package com.example.demo.service;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.model.Book;
 import com.example.demo.model.Question;
+import com.example.demo.repository.BookRepository;
+import com.example.demo.repository.QuestionRepository;
+import static com.example.demo.specification.QuestionSpecifications.*;
+
+import org.springframework.data.jpa.domain.Specification;
 
 @Service
 public class SearchquestionService {
-	public List<Question> findQuestionByQuestionKeyword(String questionKeyword){
-		return List.of();
+
+	@Autowired
+	QuestionRepository questionRepository;
+
+	@Autowired
+	BookRepository bookRepository;
+
+	public List<Question> findQuestionUsingIsbn(String isbn, String questionKeyword) {
+		return questionRepository.findAll(Specification.where(isbnIn(List.of(isbn)))
+			.and(contentContains(questionKeyword)));
+	}
+
+	public List<Question> findQuestionUsingBookKeyword(String bookKeyword, String questionKeyword) throws IOException {
+		List<String> isbnList = bookRepository.findByKeyword(bookKeyword)
+			.stream()
+			.map(Book::getIsbn13)
+			.collect(Collectors.toList());
+		return questionRepository.findAll(Specification.where(isbnIn(isbnList))
+			.and(contentContains(questionKeyword)));
 	}
 }
