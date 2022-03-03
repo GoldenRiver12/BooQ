aaa0cca9fc3ba5c9c3e29aa17db9382e17962f01
GoldenRiver12 <gandhi9736@gmail.com>
2022-03-03 19:41:51 +0900

    [modify] h2consoleへの未制限のアクセスを停止

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/login","/register","/h2-console/**").permitAll()
+				.antMatchers("/login","/register").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
