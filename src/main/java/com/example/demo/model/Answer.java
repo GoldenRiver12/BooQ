d3fad1a1d34dd52b9a940f65c6546329ce4297b8
GoldenRiver12 <gandhi9736@gmail.com>
2022-03-03 20:34:17 +0900

    [add] 回答modelとその主キークラスを作成

---
+++ src/main/java/com/example/demo/model/Answer.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+/**
+ * 質問に対する回答のモデル
+ * @author Kanagawa Tatsuki
+ *
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+@IdClass(AnswerId.class)
+public class Answer implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * 回答ID
+	 */
+	@Id
+	private Long answerId;
+	
+	/**
+	 * 回答したユーザーID
+	 */
+	@Id
+	private Long userid;
+	
+	/**
+	 * 回答先の質問ID
+	 */
+	@Id
+	@GeneratedValue
+	private Long questionId;
+	
+	/**
+	 * 回答の投稿日時
+	 */
+	private LocalDateTime registrationTime;
+	
+	/**
+	 * 回答内容
+	 */
+	private String content;
+}
