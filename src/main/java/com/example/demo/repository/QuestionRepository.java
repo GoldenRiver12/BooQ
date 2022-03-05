b832809933640c5b157de4e496eaa3b3b94f2f21
GoldenRiver12 <gandhi9736@gmail.com>
2022-03-05 20:20:42 +0900

    [fix] 存在しないクラスのimport削除

--- src/main/java/com/example/demo/repository/QuestionRepository.java
+++ src/main/java/com/example/demo/repository/QuestionRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.example.demo.model.Question;
-import com.example.demo.model.QuestionId;
 
 public interface QuestionRepository extends JpaRepository<Question, Long>, JpaSpecificationExecutor<Question> {
 	
